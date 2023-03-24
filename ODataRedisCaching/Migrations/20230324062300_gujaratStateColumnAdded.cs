88a31731d23c4531ec26a4f59d029f555c254c79
smitesh <smitesh.maniya@marutitech.com>
2023-03-24 14:12:33 +0530

    change

---
+++ ODataRedisCaching/Migrations/20230324062300_gujaratStateColumnAdded.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ODataPractice.Migrations
+{
+    /// <inheritdoc />
+    public partial class gujaratStateColumnAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GujaratStates",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    STCode = table.Column<int>(type: "int", nullable: false),
+                    StateName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DTCode = table.Column<int>(type: "int", nullable: false),
+                    DistrictName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SDTCode = table.Column<int>(type: "int", nullable: false),
+                    SubDistrictName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TownCode = table.Column<int>(type: "int", nullable: false),
+                    AreaName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GujaratStates", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GujaratStates");
+        }
+    }
+}
---
+++ ODataRedisCaching/Migrations/20230324070453_identity added.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ODataPractice.Migrations
+{
+    /// <inheritdoc />
+    public partial class identityadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "GujaratStates",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "GujaratStates",
+                newName: "ID");
+        }
+    }
+}
