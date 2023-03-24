76762e067ec8892b14de1ca4204c31885d2328b3
Amit-limbasiya <amit.limbasiya@marutitech.com>
2023-03-24 15:26:37 +0530

    ODataRedisCachingOnGujaratData

--- ODataRedisCaching/Controllers/DistrictController.cs
+++ ODataRedisCaching/Controllers/DistrictController.cs
@@ -1,11 +1,41 @@
 ﻿using Microsoft.AspNet.OData;
 using Microsoft.AspNet.OData.Routing;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using ODataRedisCaching.Models;
+using ODataRedisCaching.Services;
 
 namespace ODataRedisCaching.Controllers
 {
-    [ODataRoutePrefix("Gujarat")]
+    [ODataRoutePrefix("District")]
     public class DistrictController : ODataController
     {
-        
+        private readonly IDistrictDataService _districtDataService;
+        public DistrictController(IDistrictDataService districtDataService)
+        {
+            _districtDataService = districtDataService;
+        }
+        [HttpGet]
+        [OutputCache]
+        [EnableQuery()]
+        public async Task<ActionResult<List<District>>> Get()
+        {
+            Console.WriteLine("Get all data");
+            var data = await _districtDataService.GetDistricts();
+            return Ok(data);
+        }
+        [ODataRoute("({id})")]
+        [OutputCache]
+        [EnableQuery()]
+        public async Task<ActionResult<District>> Get(int id)
+        {
+            Console.WriteLine("Get district data");
+            var district = await _districtDataService.GetDistrictData(id);
+            if (district == null)
+            {
+                return BadRequest("District not found with given id.");
+            }
+            return Ok(district);
+        }
     }
 }
